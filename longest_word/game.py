6a257495af830f8dfe5bf6fe45db2ffd8a3b708f
Lorcan Rae <lorcanrae@gmail.com>
2023-07-27 18:57:48 +0000

    initial commit

---
+++ longest_word/game.py
@@ -0,0 +1,34 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import random
+import copy
+
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = [chr(random.randint(ord("A"), ord("Z"))) for _ in range(9)]
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+
+        # Check empty string
+        if not word:
+            return False
+
+        # Check if word is valid
+        grid_ = copy.deepcopy(self.grid)
+        for letter in word:
+            if letter in grid_:
+                grid_.remove(letter)
+            else:
+                return False
+        return True
+
+
+# if __name__ == '__main__':
+#     game = Game()
+#     game.grid = [l for l in "OQUWRBAZE"]
+#     my_word = "BAROQUE"
+#     print(game.is_valid(my_word))
---
+++ tests/test_game.py
@@ -0,0 +1,53 @@
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+
+        # setup
+        new_game = Game()
+
+        # exercise
+        grid = new_game.grid
+
+        # verify
+        assert type(grid) == list
+        assert len(grid) == 9
+        for letter in grid:
+            assert letter in string.ascii_uppercase
+
+    def test_empty_word_is_invalid(self):
+        new_game = Game()
+
+        assert new_game.is_valid("") == False
+
+    def test_is_valid(self):
+
+        # setup
+        new_game = Game()
+
+        ## set grid and test word
+        test_grid = "OQUWRBAZE"
+        test_word = "BAROQUE"
+        new_game.grid = list(test_grid)
+
+        # exercise
+        assert new_game.is_valid(test_word) == True
+
+        # verify
+        assert new_game.grid == list(test_grid)
+
+    def test_is_invalid(self):
+        # setup
+        new_game = Game()
+        test_grid = "QEULNNNNK"
+        test_word = "BAROQUE"
+
+        ## Set grid and test word
+        new_game.grid = list(test_grid)
+
+        # exercise
+        assert new_game.is_valid(test_word) == False
+
+        # verify
+        assert new_game.grid == list(test_grid)
